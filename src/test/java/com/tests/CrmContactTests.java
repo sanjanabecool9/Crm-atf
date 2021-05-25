a7b07b6a7b4323f0e31896e32dc41573c93b47bd
sanjanabecool9 <84775407+sanjanabecool9@users.noreply.github.com>
2021-05-25 15:55:19 +0530

    second commit
    second commit

--- src/test/java/com/tests/CrmContactTests.java
+++ src/test/java/com/tests/CrmContactTests.java
@@ -16,61 +16,62 @@ import com.crm.services.LoginService;
 import com.crm.validators.CrmContactValidator;
 
 public class CrmContactTests {
-	LoginService loginService=null;
-    HomeServices homeService=null;
-    CrmContactServices crmContactServices=null;
+	LoginService loginService = null;
+	HomeServices homeService = null;
+	CrmContactServices crmContactServices = null;
 
-    CrmContactData crmContactData = null;
-    List<ContactData> appsData = null;
-    
-    CrmContactValidator crmContactValidator=null;
-    
-   
+	CrmContactData crmContactData = null;
+	List<ContactData> appsData = null;
 
+	CrmContactValidator crmContactValidator = null;
 
-@BeforeClass
-public void init(){
-	 
-	loginService=new LoginService(); 
-	homeService=new HomeServices();
-	crmContactServices=new CrmContactServices();
-	crmContactData = CrmContactTestData.getTestData();
-	appsData = crmContactData.getCrmcontactdata();	
-	crmContactValidator = new CrmContactValidator();
-	    
-	InitializeViews.init();	
-	
-	BrowserDriver.getCurrentDriver().get(PropertyLoader.getQaUrl());
-}
+	@BeforeClass
+	public void init() {
 
-@Test
-public void createContact(){
-	loginService.loginToApplication(appsData.get(0).getUsername(),appsData.get(0).getPassword());
-	homeService.navigateToContacts();
-	
-	//create contact testcase
-	crmContactServices.navigateToCreateContact().selectSalutation(appsData.get(0).getSalutation());
-	crmContactServices.insertName(appsData.get(0).getFirstname(),appsData.get(0).getLastname());
-	crmContactServices.insertEmail(appsData.get(0).getEmail());
-	crmContactServices.insertDescription();
-	crmContactServices.inserAssignedUser();
-	crmContactServices.clickSaveContact();
-	crmContactServices.getCreatedEmail();
-	crmContactValidator.validateContactName(crmContactServices);
-	crmContactValidator.validateCreatedEmail(crmContactServices);
-	crmContactValidator.validateCreatedAssignedUser(crmContactServices);
-	
-	//edit contact testcase
-	crmContactServices.editContact();
-	crmContactServices.editContactEmail(appsData.get(0).getEditemail());
-	crmContactServices.clickSaveContact();
-	crmContactServices.getEditedEmail();
-	crmContactValidator.validateEditedEmail(crmContactServices);
-	
-	//remove contact testcase
-	crmContactServices.removeContact();
-	homeService.logoutOfCrm();
+		loginService = new LoginService();
+		homeService = new HomeServices();
+		crmContactServices = new CrmContactServices();
+		crmContactData = CrmContactTestData.getTestData();
+		appsData = crmContactData.getCrmcontactdata();
+		crmContactValidator = new CrmContactValidator();
 
+		InitializeViews.init();
+
+		BrowserDriver.getCurrentDriver().get(PropertyLoader.getQaUrl());
+	}
+
+	@Test
+	public void createContact() {
+		loginService.loginToApplication(appsData.get(0).getUsername(), appsData.get(0).getPassword());
+		homeService.navigateToContacts();
+
+		// create contact testcase
+		crmContactServices.navigateToCreateContact().selectSalutation(appsData.get(0).getSalutation());
+		crmContactServices.insertName(appsData.get(0).getFirstname(), appsData.get(0).getLastname());
+		crmContactServices.insertEmail(appsData.get(0).getEmail());
+		crmContactServices.insertDescription();
+		crmContactServices.inserAssignedUser();
+		crmContactServices.clickSaveContact();
+		crmContactServices.getCreatedEmail();
+		crmContactValidator.validateContactName(crmContactServices);
+		crmContactValidator.validateCreatedEmail(crmContactServices);
+		crmContactValidator.validateCreatedAssignedUser(crmContactServices);
+
+		// edit contact testcase
+		crmContactServices.editContact();
+		crmContactServices.editContactEmail(appsData.get(0).getEditemail());
+		crmContactServices.clickSaveContact();
+		crmContactServices.getEditedEmail();
+		crmContactValidator.validateEditedEmail(crmContactServices);
+
+		// remove contact testcase
+		crmContactServices.removeContact();
+		homeService.logoutOfCrm();
+
+	}
 	
-}
+	@Test(description = "This is gitHub Sample Test Case")
+	public void gitHubSampleTestCase() {
+		
+	}
 }
